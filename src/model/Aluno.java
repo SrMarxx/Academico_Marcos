0780ebbe59e585eb2a6e8ca52efe17c2ac198052
srmarxx <marcosgabrieldiascardoso66@gmail.com>
2024-08-28 04:21:34 -0300

    Adicionando metodos novos na classe Aluno

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,17 +1,46 @@
 package model;
 
-public class Aluno extends Pessoa{
-    private int nMatricula;
-    private Curso curso;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
-    public Aluno(String nome, String email, boolean sexo, int nMatricula, Curso curso) {
+public class Aluno extends Pessoa{
+    private String nMatricula;
+    private Curso curso;
+    private ArrayList<Nota> notas;
+    private static int numSequencial;
+    private static LocalDate dataAtual = LocalDate.of(2024, 4, 9);
+
+    public Aluno(String nome, String email, boolean sexo, Curso curso) {
         super(nome, email, sexo);
-        this.nMatricula = nMatricula;
+        this.nMatricula = geraMatricula();
         this.curso = curso;
+        this.notas = new ArrayList<Nota>();
     }
 
     @Override
     public String toString() {
         return nMatricula + " - " + nome + "\n("+email+")";
+    }
+
+    public String getNome() {
+        String nomeNovo = String.copyValueOf(this.nome.toCharArray());
+        return nomeNovo;
+    }
+    public String getMatricula() {
+        String matriculaNovo = String.copyValueOf(this.nMatricula.toCharArray());
+        return matriculaNovo;
+    }
+    public String getEmail() {
+        String emailNovo = String.copyValueOf(this.email.toCharArray());
+        return emailNovo;
+    }
+
+    private String geraMatricula() {
+        if(Aluno.dataAtual.getYear() != LocalDate.now().getYear()) {
+            Aluno.numSequencial = 0;
+            Aluno.dataAtual = LocalDate.now();
+        }
+        Aluno.numSequencial++;
+        return dataAtual.getYear() + "-" + Aluno.numSequencial;
     }
 }
